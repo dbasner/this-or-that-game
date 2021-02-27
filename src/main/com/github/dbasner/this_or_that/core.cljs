e440841391c8e6d6d93f0027e3a7acd8aee8700b
Daniel Basner <dbasner@gmail.com>
2021-02-27 13:28:19 -0500

    add basic proof-of-running, shows hello in html and logs an init message to browser console

--- src/main/com/github/dbasner/this_or_that/core.cljs
+++ src/main/com/github/dbasner/this_or_that/core.cljs
@@ -1,6 +1,11 @@
-(ns com.github.dbasner.this-or-that.core)
+(ns com.github.dbasner.this-or-that.core
+  (:require
+    [goog.functions :as gfunctions]
+    [oops.core :refer [ocall]]))
 
-(defn hello-world
-  "prints hello world"
-  []
- (print "hello world"))
+(def init!
+  (gfunctions/once
+    (fn []
+      (js/console.log "Initializing This-Or-That!!!!"))))
+
+(ocall js/window "addEventListener" "load" init!)
