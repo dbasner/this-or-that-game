cb04f12c0fea086f3a8a458ceaa49368baa8f278
Daniel Basner <dbasner@gmail.com>
2021-03-16 19:52:07 -0400

    add new words

--- src/main/com/github/dbasner/this_or_that/game_logic.cljs
+++ src/main/com/github/dbasner/this_or_that/game_logic.cljs
@@ -10,12 +10,25 @@
 
 ;; start by doing just generating a basic string
 
-(def verbs [
-            "eat" "cook" "punch" "get punched by" "get kicked by" "kick" "date" "kiss" "marry"])
+(def verbs ["eat" "cook" "punch" "get punched by"
+            "get kicked by" "kick" "date" "kiss"
+            "marry" "love" "get laughed at by"
+            "live with" "barbecue" "get yelled at by"
+            "upset" "bother" "take a roadtrip with"
+            "fly sitting next to" "fall for"
+            "get trapped on a desert island with"
+            "become conjoined twins with"
+            "be trapped with" "be looked up to by"
+            "settle down with" "burn" "get wasted with"])
 
 (def count-modifiers ["roughly" "about" "precisely" "exactly" "almost" "just about"])
 
-(def nouns ["sharks" "people" "dogs" "turles" "chairs" "tables"])
+(def nouns ["sharks" "people" "dogs" "turtles"
+            "chairs" "tables" "dinosaurs" "computers"
+            "men" "women" "cats" "rats" "couches"
+            "airplanes" "cars" "haters" "phones"
+            "jugglers" "clowns" "doctors" "therapists"
+            "lawyers" "artists" "hipsters"])
 
 (defn rand-verb [] (rand-nth verbs))
 (defn rand-count-modifier [] (rand-nth count-modifiers))
